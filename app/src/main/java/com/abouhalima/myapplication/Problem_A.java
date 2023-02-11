7ec684741390a9edf74b61e879f47d01104ad76d
DESKTOP-1UNUI5H\p0p <gamalabdallah005@gmail.com>
2023-02-11 19:54:12 +0200

    first commit!!

---
+++ app/src/main/java/com/abouhalima/myapplication/Problem_A.java
@@ -0,0 +1,13 @@
+package com.abouhalima.myapplication;
+
+
+import java.util.Scanner;
+
+public class Problem_A {
+    public static void main(String[] args) {
+        Scanner S=new Scanner(System.in);
+        String a=S.nextLine();
+        System.out.println("Hello, "+a);
+
+    }
+}
