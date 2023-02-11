bd2d692968d7f1c87c5c193c3b7a08f2128e51c4
DESKTOP-1UNUI5H\p0p <gamalabdallah005@gmail.com>
2023-02-11 22:35:43 +0200

    Second commit!!

--- app/src/main/java/com/abouhalima/myapplication/Problem_B.java
+++ app/src/main/java/com/abouhalima/myapplication/Problem_B.java
@@ -1,7 +1,15 @@
 package com.abouhalima.myapplication;
 
+import java.util.Scanner;
+
 public class Problem_B {
     public static void main(String[] args) {
-        System.out.println("hhhhhhhhh");
+        Scanner scan=new Scanner(System.in);
+        int x=scan.nextInt();
+        long g=scan.nextLong();
+        char y=scan.next().charAt(0);
+        float z=scan.nextFloat();
+        double f=scan.nextDouble();
+        System.out.println(x+"\n"+g+"\n"+y+"\n"+z+"\n"+f);
     }
 }
