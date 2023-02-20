b3ed83030a765ee3750b59ca8c8793ebbdc4175d
DESKTOP-1UNUI5H\p0p <gamalabdallah005@gmail.com>
2023-02-20 19:51:39 +0200

    problem-N commit!!

--- app/src/main/java/com/abouhalima/myapplication/Problem_N.java
+++ app/src/main/java/com/abouhalima/myapplication/Problem_N.java
@@ -1,7 +1,24 @@
 package com.abouhalima.myapplication;
 
+import java.util.Scanner;
+
 public class Problem_N {
-    public static void main(String[] args) {
-        
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            char mychar = scan.next().charAt(0);
+            int asc=mychar;
+            if(asc >= 97 && asc <= 122)
+            {
+                asc=asc-32;
+                char rechar= (char) asc;
+                System.out.println(rechar);
+            }else if(asc >= 65 && asc <=90)
+            {
+                asc=asc+32;
+                char rechar= (char) asc;
+                System.out.println(rechar);
+
+        }
     }
 }
