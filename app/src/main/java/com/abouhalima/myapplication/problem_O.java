94df4e452cfaf6a2b8e7c223c328ae6bbba09e42
DESKTOP-1UNUI5H\p0p <gamalabdallah005@gmail.com>
2023-02-20 22:49:15 +0200

    problem-O commit!!

--- app/src/main/java/com/abouhalima/myapplication/problem_O.java
+++ app/src/main/java/com/abouhalima/myapplication/problem_O.java
@@ -3,5 +3,48 @@ package com.abouhalima.myapplication;
 import java.util.Scanner;
 
 public class problem_O {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String equ=scan.nextLine();
+        int indexOperator = 0;
+        char charOperator='+';
+        String num1,num2;
+        for(int i=0;i<equ.length();i++)
+        {
+            if(equ.charAt(i)=='+'
+                    ||equ.charAt(i)=='-'
+                    ||equ.charAt(i)=='*'
+                    || equ.charAt(i)=='/')
+            {
+                    indexOperator=i; //2
+                    charOperator=equ.charAt(i); //+,-,*,/
+                    break;
+            }
+        }
+        num1=equ.substring(0,indexOperator);
+        num2=equ.substring(indexOperator+1);
 
+        int num1Int=Integer.parseInt(num1);
+        int num2Int=Integer.parseInt(num2);
+
+
+        switch (charOperator)
+        {
+            case '+':
+                System.out.println(num1Int+num2Int);
+                break;
+            case '-':
+                System.out.println(num1Int-num2Int);
+                break;
+            case '*':
+                System.out.println(num1Int*num2Int);
+                break;
+            case '/':
+                System.out.println(num1Int/num2Int);
+                break;
+        }
+
+
+
+    }
 }
